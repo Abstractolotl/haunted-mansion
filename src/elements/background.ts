1174e179663bb797af7b65d8c554815aec8c5761
dseydel <daniel.seydel@ionos.com>
2024-08-09 09:41:24 +0200

    Add item selection functionality

--- src/elements/background.ts
+++ src/elements/background.ts
@@ -9,14 +9,20 @@ export default class Background {
     private gridConfig: GridConfig;
     private grid: Grid;
 
+    private inventorySize: { rows: number; slotSize: number[]; };
+    private inventoryStartX: number = 0;
+
     constructor(gridConfig: GridConfig, config: ConfigHelper, parent: HTMLElement) {
         this.gridConfig = gridConfig;
         this.parent = parent;
 
+        this.inventorySize = config.getInventorySize();
+        this.inventoryStartX = config.getSceneBorder()[0];
+
         this.grid = new Grid(this.gridConfig, 'background');
         this.parent.appendChild(this.grid.getElement());
 
-        this.drawInventoryBorder(config.getInventorySize(), config.getSceneBorder()[0])
+        this.drawInventoryBorder()
         this.drawGameBorder(config.getSceneBorder())
     }
 
@@ -43,46 +49,63 @@ export default class Background {
         this.draw('.', this.gridConfig.width - 1, this.gridConfig.height - 1);
     }
 
-    private drawInventoryBorder(inventorySize: { rows: number; slotSize: number[]; }, actionLogStartX: number = 0) {
-        let maxX = actionLogStartX - 2;
+    public drawInventoryBorder(activeSlot?: number) {
+        let maxX = this.inventoryStartX - 2;
         let start = {x: 2, y: 1}
 
-        let columns = Math.floor((maxX - start.x) / (inventorySize.slotSize[0] + 1))
+        let columns = Math.floor((maxX - start.x) / (this.inventorySize.slotSize[0] + 1))
 
-        for (let y = 0; y < inventorySize.rows; y++) {
+        for (let y = 0; y < this.inventorySize.rows; y++) {
             for (let x = 0; x < columns; x++) {
+                let slot = (y * columns) + x;
+                let borderY = '|'; let borderX = '-';
+                if (activeSlot !== undefined && slot === activeSlot) {
+                    borderY = '#'
+                    borderX = '='
+                }
+
                 this.draw('+', start.x, start.y)
-                this.fill({x: start.x + 1, y: start.y}, {x: start.x + inventorySize.slotSize[0], y: start.y}, '-')
-                this.draw('+', start.x + inventorySize.slotSize[0] + 1, start.y)
+                this.fill({x: start.x + 1, y: start.y}, {x: start.x + this.inventorySize.slotSize[0], y: start.y}, '-')
+                this.draw('+', start.x + this.inventorySize.slotSize[0] + 1, start.y)
 
-                this.fill({x: start.x, y: start.y + 1}, {x: start.x, y: start.y + inventorySize.slotSize[1]}, '|')
-                this.fill({x: start.x + inventorySize.slotSize[0] + 1, y: start.y + 1}, {x: start.x + inventorySize.slotSize[0] + 1, y: start.y + inventorySize.slotSize[1]}, '|')
+                this.fill({x: start.x, y: start.y + 1}, {x: start.x, y: start.y + this.inventorySize.slotSize[1]}, '|')
+                this.fill({x: start.x + this.inventorySize.slotSize[0] + 1, y: start.y + 1}, {x: start.x + this.inventorySize.slotSize[0] + 1, y: start.y + this.inventorySize.slotSize[1]}, '|')
 
-                this.draw('+', start.x, start.y + inventorySize.slotSize[1] + 1)
-                this.fill({x: start.x + 1, y: start.y + inventorySize.slotSize[1] + 1}, {x: start.x + inventorySize.slotSize[0], y: start.y + inventorySize.slotSize[1] + 1}, '-')
-                this.draw('+', start.x + inventorySize.slotSize[0] + 1, start.y + inventorySize.slotSize[1] + 1)
+                this.draw('+', start.x, start.y + this.inventorySize.slotSize[1] + 1)
+                this.fill({x: start.x + 1, y: start.y + this.inventorySize.slotSize[1] + 1}, {x: start.x + this.inventorySize.slotSize[0], y: start.y + this.inventorySize.slotSize[1] + 1}, '-')
+                this.draw('+', start.x + this.inventorySize.slotSize[0] + 1, start.y + this.inventorySize.slotSize[1] + 1)
 
-                start.x += inventorySize.slotSize[0] + 1;
+                start.x += this.inventorySize.slotSize[0] + 1;
             }
             start.x = 2;
-            start.y += inventorySize.slotSize[1] + 1;
+            start.y += this.inventorySize.slotSize[1] + 1;
+        }
+
+        if (activeSlot !== undefined) {
+            let x = (activeSlot % columns) * (this.inventorySize.slotSize[0] + 1) + 2;
+            let y = Math.floor(activeSlot / columns) * (this.inventorySize.slotSize[1] + 1) + 1;
+
+            this.fill({x: x, y: y + 1}, {x: x, y: y + this.inventorySize.slotSize[1]}, '#')
+            this.fill({x: x + this.inventorySize.slotSize[0] + 1, y: y + 1}, {x: x + this.inventorySize.slotSize[0] + 1, y: y + this.inventorySize.slotSize[1]}, '#')
+            this.fill({x: x + 1, y: y}, {x: x + this.inventorySize.slotSize[0], y: y}, '=')
+            this.fill({x: x + 1, y: y + this.inventorySize.slotSize[1] + 1}, {x: x + this.inventorySize.slotSize[0], y: y + this.inventorySize.slotSize[1] + 1}, '=')
         }
 
     }
 
     // TODO: where should the border actually be drawn? What layer should it be on?
-    public drawBorder(sizeX: number, sizeY: number, inventoryheight: number, actionLogWidth: number) {
+    public drawBorder(sizeX: number, sizeY: number, inventoryHeight: number, actionLogWidth: number) {
         // draw horizontal lines
         for (let x = 0; x < sizeX + actionLogWidth + 1; x++) {
             this.draw('-', x, 0);
-            this.draw('-', x, inventoryheight + sizeY + 2);
+            this.draw('-', x, inventoryHeight + sizeY + 2);
         }
         for (let x = 1; x < sizeX + 1; x++) {
-            this.draw('-', x, inventoryheight + 1);
+            this.draw('-', x, inventoryHeight + 1);
         }
 
         // draw vertical lines
-        for (let y = 1; y < sizeY + inventoryheight + 2; y++) {
+        for (let y = 1; y < sizeY + inventoryHeight + 2; y++) {
             this.draw('|', 0, y);
             this.draw('|', sizeX, y);
             this.draw('|', sizeX + actionLogWidth + 1, y);
@@ -95,13 +118,13 @@ export default class Background {
         this.draw('+', sizeX + actionLogWidth + 1, 0);
 
         // middle row
-        this.draw('+', 0, inventoryheight + 1);
-        this.draw('+', sizeX, inventoryheight + 1);
+        this.draw('+', 0, inventoryHeight + 1);
+        this.draw('+', sizeX, inventoryHeight + 1);
 
         // top row
-        this.draw('+', 0, sizeY + inventoryheight + 2);
-        this.draw('+', sizeX, sizeY + inventoryheight + 2);
-        this.draw('+', sizeX + actionLogWidth + 1, sizeY + inventoryheight + 2);
+        this.draw('+', 0, sizeY + inventoryHeight + 2);
+        this.draw('+', sizeX, sizeY + inventoryHeight + 2);
+        this.draw('+', sizeX + actionLogWidth + 1, sizeY + inventoryHeight + 2);
     }
 
     public getGrid(){
